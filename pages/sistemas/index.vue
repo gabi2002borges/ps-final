4300acac5d017f2fcd27fb6a50300ad812682015
gabi2002borges <gb54081profissional@gmail.com>
2022-01-24 11:37:35 -0300

    24.01

--- pages/sistemas/index.vue
+++ pages/sistemas/index.vue
@@ -9,7 +9,7 @@
                     Sistema de pesquisa de satisfação dos alunos e professores sobre o senai
                 </p>
                 <div class="btn-sistemas">
-                    <router-link to="/">OK</router-link>
+                    <router-link to="/"  class="btn" >OK</router-link>
                 </div>
             </div>
 
@@ -21,7 +21,7 @@
                     Formulario FIAP para declarar alunos com baixo desempenho
                 </p>
                 <div class="btn-sistemas">
-                    <router-link to="/">OK</router-link>
+                    <router-link to="/"  class="btn">OK</router-link>
                 </div>
             </div>
         </div>
@@ -41,6 +41,7 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    
 }
 
 .container{
@@ -49,25 +50,50 @@ export default {
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
+    background-color: #E8E6E6;
 }
 
 .cards {
     padding: 20px;
     width: auto;
-
+    
     #card {
-      height: 250px;
-      width: 250px;
-      border: 1px solid;
-      border-radius: 25px;
+      height: 369px;
+      width: 300px;
       display: flex;
       flex-direction: column;
       justify-content: center;
+      background-color: #F4F4F4;
+
+      img{
+          height: 250px;
+          width: 300px;
+      }
     }
 
     .card-body {
       margin-top: 20px;
       text-align: center;
+    }
+
+    .btn-sistemas{
+        height: 40px;
+        width: 250px;
+        background-color: #EC1C24;
+        position: relative;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        align-items: center !important;
+
+        .btn{
+            background-color: #EC1C24;
+            color: white;
+            text-decoration: none;
+            font-size: 18px;
+
+        }
     }
 }
 </style>
